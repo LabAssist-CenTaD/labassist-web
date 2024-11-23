c1f010f3ed50f6ec4b6a392f72c2683bf701794c
yun297 <zhuyunsong02097007@gmail.com>
2024-11-23 21:08:01 +0800

    Remove global styles and update App component imports; add background color to body in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // import React from "react";
-import "./styles/global.css"; // Import global styles
 import "./styles/variables.css"; // Import global variables
 import "./styles/fonts.css"; // Import custom fonts
 
