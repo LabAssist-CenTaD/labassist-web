eb7e680cc65466dfec336a06c8e51c95622cfc9f
yun297 <zhuyunsong02097007@gmail.com>
2024-11-24 22:19:41 +0800

    Refactor toolbar by removing Button40 component and adding AddButton; update styles for consistency

---
+++ src/components/LeftPanel/PredictionList/Toolbar/AddButton/AddButton.tsx
@@ -0,0 +1,14 @@
+import { AddCircle } from "iconsax-react";
+import "./AddButton.css";
+
+export const AddButton = (): JSX.Element => {
+  const handleClick = () => {
+    console.log("Add button clicked");
+  };
+
+  return (
+    <button className="add-button" onClick={handleClick}>
+      <AddCircle size={16} variant="Bold" color="rgba(0, 23, 31, 1)" />
+    </button>
+  );
+};
--- src/components/LeftPanel/PredictionList/Toolbar/Toolbar.tsx
+++ src/components/LeftPanel/PredictionList/Toolbar/Toolbar.tsx
@@ -2,15 +2,13 @@
 import "./Toolbar.css";
 import { Search } from "./Search/Search";
 import { SelectButton } from "./SelectButton/SelectButton";
-import { Button40 } from "./Button40/Button40";
-import { FolderAdd, VideoAdd } from "iconsax-react";
+import { AddButton } from "./AddButton/AddButton";
 
 export const Toolbar = (): JSX.Element => {
   return (
     <div className="toolbar">
       <Search />
-      <Button40 Icon={VideoAdd} />
-      <Button40 Icon={FolderAdd} />
+      <AddButton />
       <SelectButton />
     </div>
   );
