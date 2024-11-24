49bd64e08af7743e04550ce0cc79fc92dbbfb9b5
yun297 <zhuyunsong02097007@gmail.com>
2024-11-24 20:37:24 +0800

    Enhance RemoveButton styling and add click handler for improved interaction

--- src/components/LeftPanel/PredictionList/CardWrapper/Card/CardDetails/ButtonWrapper/RemoveButton/RemoveButton.tsx
+++ src/components/LeftPanel/PredictionList/CardWrapper/Card/CardDetails/ButtonWrapper/RemoveButton/RemoveButton.tsx
@@ -2,8 +2,12 @@ import { Trash } from "iconsax-react";
 import "./RemoveButton.css";
 
 export const RemoveButton = (): JSX.Element => {
+  const handleClick = () => {
+    console.log("Remove button clicked");
+  };
+
   return (
-    <button className="remove-button">
+    <button className="remove-button" onClick={handleClick}>
       <Trash size={16} variant="Bold" color="rgba(0, 23, 31, 1)" />
     </button>
   );
