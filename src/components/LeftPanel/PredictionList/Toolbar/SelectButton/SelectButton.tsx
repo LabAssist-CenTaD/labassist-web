6eab5d57bcbfcd291e0d239a92babecbd4db3220
yun297 <zhuyunsong02097007@gmail.com>
2024-11-24 15:55:41 +0800

    Refactor SelectButton CSS class names for consistency

--- src/components/LeftPanel/PredictionList/Toolbar/SelectButton/SelectButton.tsx
+++ src/components/LeftPanel/PredictionList/Toolbar/SelectButton/SelectButton.tsx
@@ -21,7 +21,7 @@ export const SelectButton = (): JSX.Element => {
       ) : (
         <TickCircle size={16} variant="Bold" color="rgba(0, 23, 31, 1)" />
       )}
-      <div className="text-wrapper">
+      <div className="select-button-text">
         {isClicked ? "Cancel" : "Select"} {/* Change text based on isClicked */}
       </div>
     </button>
