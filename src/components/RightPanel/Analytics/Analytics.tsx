55cc23d476088f13b6ecb509b9a0f79d8461f6a8
yun297 <zhuyunsong02097007@gmail.com>
2024-11-26 12:26:22 +0800

    Refactor Analytics component to remove VideoPlayer; restructure PredictionTimeline layout and styles; add VideoPlayer to PredictionTimeline

--- src/components/RightPanel/Analytics/Analytics.tsx
+++ src/components/RightPanel/Analytics/Analytics.tsx
@@ -1,16 +1,12 @@
 import "./Analytics.css";
 import { AnalyticsHeader } from "./AnalyticsHeader/AnalyticsHeader";
 import { PredictionTimeline } from "./PredictionTimeline/PredictionTimeline";
-import { VideoPlayer } from "./VideoPlayer/VideoPlayer";
 
 export const Analytics = (): JSX.Element => {
   return (
     <div className="analytics">
       <AnalyticsHeader />
-      <div className="analytics-container">
-        <VideoPlayer />
-        <PredictionTimeline />
-      </div>
+      <PredictionTimeline />
     </div>
   );
 };
--- src/components/RightPanel/Analytics/PredictionTimeline/PredictionTimeline.tsx
+++ src/components/RightPanel/Analytics/PredictionTimeline/PredictionTimeline.tsx
@@ -1,13 +1,17 @@
 import "./PredictionTimeline.css";
 import { PredictionFilter } from "./PredictionFilter/PredictionFilter";
 import { Timeline } from "./Timeline/Timeline";
+import { VideoPlayer } from "./VideoPlayer/VideoPlayer";
 
 export const PredictionTimeline = (): JSX.Element => {
   return (
     <div className="prediction-timeline">
-      <div className="prediction-timeline-header">Prediction Timeline</div>
-      <PredictionFilter />
-      <Timeline />
+      <VideoPlayer />
+      <div className="prediction-timeline-right-container">
+        <div className="prediction-timeline-header">Prediction Timeline</div>
+        <PredictionFilter />
+        <Timeline />
+      </div>
     </div>
   );
 };
