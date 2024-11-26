da0579a2714e5dbd3823087f0c5440a4e252eead
yun297 <zhuyunsong02097007@gmail.com>
2024-11-26 13:16:21 +0800

    Refactor Timeline component for improved readability; adjust TimelineEntry layout and CSS for better responsiveness

--- src/components/RightPanel/Analytics/VideoMode/PredictionTimeline/Timeline/Timeline.tsx
+++ src/components/RightPanel/Analytics/VideoMode/PredictionTimeline/Timeline/Timeline.tsx
@@ -2,7 +2,13 @@ import "./Timeline.css";
 import { TimelineEntry } from "./TimelineEntry/TimelineEntry";
 
 export const Timeline = (): JSX.Element => {
-  return <div className="timeline">
-    <TimelineEntry type="info" timestamp="00:00:00" message="This is an info message" />
-  </div>;
+  return (
+    <div className="timeline">
+      <TimelineEntry
+        type="info"
+        timestamp="00:00:00"
+        message="This is an info message"
+      />
+    </div>
+  );
 };
