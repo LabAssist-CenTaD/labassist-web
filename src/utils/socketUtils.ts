12df53aec2d0e55414ee234c982d88d3b88e263d
yun297 <zhuyunsong02097007@gmail.com>
2024-12-02 23:46:10 +0800

    Refactor socket utility functions to implement singleton pattern and enhance event handling

--- src/utils/socketUtils.ts
+++ src/utils/socketUtils.ts
@@ -1,4 +1,4 @@
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 import * as jsonpatch from "fast-json-patch";
 import { TagStatus } from "../components/LeftPanel/PredictionList/CardWrapper/Card/StatusBar/TagWrapper/Tag/Tag";
 
@@ -20,39 +20,54 @@ type FileData = {
   };
 };
 
-const socket = io("http://localhost:5000");
-let jsonData = {};
+let socket: Socket | null = null; // Singleton socket instance
+let jsonData = {}; // Stores the current state
 
-socket.on("delta_update", (patch) => {
-  jsonpatch.applyPatch(jsonData, patch);
-});
+// Initialise the socket connection
+export function initSocket() {
+  if (!socket) {
+    socket = io("http://localhost:5000");
 
+    // Socket events
+    socket.on("connect", () => {
+      console.log("Connected to server");
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+    });
+
+    socket.on("delta_update", (patch) => {
+      jsonpatch.applyPatch(jsonData, patch);
+    });
+
+    socket.on("patch_frontend", (data) => {
+      console.log("Message from server:", data);
+    });
+
+    socket.on("authentication_success", (data) => {
+      console.log("Authentication success:", data);
+    });
+
+    console.log("Socket initialised");
+  }
+
+  return socket;
+}
+
+// Emit updates to the server
 export function sendUpdate(newData: FileData[]): void {
-  // JSON type
+  const socketInstance = initSocket();
   const patch = jsonpatch.compare(jsonData, newData);
-  socket.emit("delta_update", patch);
+  socketInstance.emit("delta_update", patch);
   jsonData = newData; // Update local data
 }
 
+// Authenticate the socket connection
 export function authenticateSocket(deviceId: string): void {
-  socket.emit("authenticate", { client_id: deviceId });
-}
-
-// Initialise the socket connection
-export function initSocket(): void {
-  socket.on("connect", () => {
-    console.log("Connected to server");
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Disconnected from server");
-  });
-
-  socket.on("message", (data) => {
-    console.log("Message from server:", data);
-  });
-
-  socket.on("authentication_success", (data) => {
-    console.log("Authentication success:", data);
-  });
+  if (socket) {
+    socket.emit("authenticate", { client_id: deviceId });
+  } else {
+    console.error("Socket is not initialised");
+  }
 }
