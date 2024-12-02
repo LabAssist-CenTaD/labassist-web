d5f9bc7624cade00f50e363a56324871304b3665
yun297 <zhuyunsong02097007@gmail.com>
2024-12-02 10:26:02 +0800

    Enhance socket utility to support file data updates; define FileData and Annotation interfaces for structured data handling

--- src/utils/socketUtils.ts
+++ src/utils/socketUtils.ts
@@ -1,5 +1,24 @@
 import { io } from "socket.io-client";
 import * as jsonpatch from "fast-json-patch";
+import { TagStatus } from "../components/LeftPanel/PredictionList/CardWrapper/Card/StatusBar/TagWrapper/Tag/Tag";
+
+interface Annotation {
+  status: string;
+  message: string;
+  timestamp: string;
+}
+
+interface FileData {
+  fileName: string;
+  filePath: string;
+  status_list: TagStatus[];
+  annotations: Annotation[];
+  status_counts: {
+    correct: number;
+    warning: number;
+    error: number;
+  };
+}
 
 const socket = io("http://localhost:5000");
 let jsonData = {};
@@ -8,7 +27,8 @@ socket.on("delta_update", (patch) => {
   jsonpatch.applyPatch(jsonData, patch);
 });
 
-export function sendUpdate(newData: JSON): void { // JSON type
+export function sendUpdate(newData: FileData[]): void {
+  // JSON type
   const patch = jsonpatch.compare(jsonData, newData);
   socket.emit("delta_update", patch);
   jsonData = newData; // Update local data
