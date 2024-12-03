a0096d18cdfc4246fe0c9e9fe75d2603cab5cdca
yun297 <zhuyunsong02097007@gmail.com>
2024-12-03 15:50:22 +0800

    Integrate useCachedVideoManager into PredictionList component to manage loading state and improve data readiness handling

--- src/components/LeftPanel/PredictionList/PredictionList.tsx
+++ src/components/LeftPanel/PredictionList/PredictionList.tsx
@@ -4,14 +4,14 @@ import { CardWrapper } from "./CardWrapper/CardWrapper";
 import { Toolbar } from "./Toolbar/Toolbar";
 import { Filter } from "./Filter/Filter";
 import { getCachedVideoManager } from "../../../utils/socketUtils";
+import { useCachedVideoManager } from "../../../hooks/useCachedVideoManager";
 
 export const PredictionList = (): JSX.Element => {
   const [searchQuery, setSearchQuery] = useState("");
   const cvm = getCachedVideoManager();
+  const dataReady = useCachedVideoManager();
 
-  const fileData = cvm.getCachedVideos();
-
-  const filteredFileData = fileData.filter((file) => {
+  const filteredFileData = cvm.getCachedVideos().filter((file) => {
     // Remove the .mp4, .mov or .avi extension and compare just the filename
     const fileNameWithoutExtension = file.fileName
       .replace(/\.(mp4|mov|avi)$/, "")
@@ -30,6 +30,10 @@ export const PredictionList = (): JSX.Element => {
   const handleSearch = (query: string) => setSearchQuery(query);
 
   // console.log("Filtered file data: ", filteredFileData);
+
+  if (!dataReady) {
+    return <div className="header">Loading...</div>; // Show loading message if data is still being fetched
+  }
 
   return (
     <div className="prediction-list">
