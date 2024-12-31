7e000a0c09dede29969bc2568863829ba4c7bb37
yun297 <zhuyunsong02097007@gmail.com>
2024-12-31 11:42:44 +0800

    Remove unused selectedFilePath

--- src/shared/selectedFile.ts
+++
@@ -1,7 +0,0 @@
-// selectedFilePath.ts
-export let selectedFilePath = "";
-
-// Function to update the selected file path
-export const setSelectedFilePath = (filePath: string) => {
-  selectedFilePath = filePath;
-};
