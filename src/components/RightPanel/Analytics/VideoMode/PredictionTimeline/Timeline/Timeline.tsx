501b629fa8d6e1ec83fb6c5d07ae68ffbe3f74b8
yun297 <zhuyunsong02097007@gmail.com>
2025-01-05 18:04:49 +0800

    Refactor time formatting logic into a utility function

--- src/components/RightPanel/Analytics/VideoMode/PredictionTimeline/Timeline/Timeline.tsx
+++ src/components/RightPanel/Analytics/VideoMode/PredictionTimeline/Timeline/Timeline.tsx
@@ -9,16 +9,7 @@ import { HorizontalSeperator } from "../../../../../HorizontalSeperator/Horizont
 import { useSelectedFileContext } from "../../../../../../hooks/useSelectedFileContext";
 import { useCachedVideoContext } from "../../../../../../hooks/useCachedVideoContext";
 import { Annotation } from "../../../../../../types/jsondata";
-
-function formatTime(seconds: number): string {
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
-
-  const formattedMinutes = minutes.toString().padStart(2, "0");
-  const formattedSeconds = remainingSeconds.toString().padStart(2, "0");
-
-  return `${formattedMinutes}:${formattedSeconds}`;
-}
+import { formatTime } from "../../../../../../utils/timeUtils";
 
 interface TimelineProps {
   activeLabels: string[]; // Receive active labels
