d8dcfb3e2726ab1f2e77b245ec875f13568aafc8
yun297 <zhuyunsong02097007@gmail.com>
2025-01-05 20:14:05 +0800

    Update FilePathDisplay to use local state for file name and reflect selected file changes

--- src/components/RightPanel/FilePathDisplay/FilePathDisplay.tsx
+++ src/components/RightPanel/FilePathDisplay/FilePathDisplay.tsx
@@ -1,15 +1,23 @@
 import "./FilePathDisplay.css";
+
 import { useSelectedFileContext } from "../../../hooks/useSelectedFileContext";
+import { useEffect, useState } from "react";
 
 export const FilePathDisplay = (): JSX.Element => {
   const { selectedFile } = useSelectedFileContext();
 
+  const [fileName, setFileName] = useState<string>(
+    selectedFile?.fileName || ""
+  );
+
+  useEffect(() => {
+    setFileName(selectedFile.fileName || "");
+  }, [selectedFile, fileName]);
+
   return (
     <div className="file-path-display">
       <div className="file-path-header">Selected Video Path:</div>
-      <div className="file-path">
-        {selectedFile.filePath || "No file selected"}
-      </div>
+      <div className="file-path">{fileName || "No file selected"}</div>
     </div>
   );
 };
