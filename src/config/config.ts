b85be830a0e9c548eb02385a153febe0b5d29a82
yun297 <zhuyunsong02097007@gmail.com>
2025-01-06 11:09:22 +0800

    Enhance config type definition with debug level and connection settings

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,9 @@
-export const config = {
+export const config: {
+  connection_address: string;
+  max_cache_size: number;
+  debug_level: 1 | 2;
+} = {
   connection_address: "http://localhost:5000",
   max_cache_size: 5, // Default cache size set to 5
+  debug_level: 1, // Default debug level set to 1
 };
