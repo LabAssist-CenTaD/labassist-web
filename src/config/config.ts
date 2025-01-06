0a224151bce2fb0e26671941e316936958f0d4d4
yun297 <zhuyunsong02097007@gmail.com>
2025-01-06 19:15:12 +0800

    Enhance configuration: switch connection address to use environment variable for better flexibility

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,8 @@ export const config: {
   debug_warnings: boolean;
   debug_errors: boolean;
 } = {
-  connection_address: "http://localhost:5000",
+  // connection_address: "http://localhost:5000",
+  connection_address: import.meta.env.VITE_API_ENDPOINT,
   max_cache_size: 5, // Default cache size set to 5
   debug_level: 1, // Default debug level set to 1
   debug_warnings: true,
